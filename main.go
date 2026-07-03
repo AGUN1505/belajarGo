99bef4b5916f0bcbf44e975e4bc1a9186bd7c5e7
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2026-07-03 22:17:48 +0700

    hapus komentar

--- main.go
+++ main.go
@@ -7,11 +7,8 @@ var namaBelakang string
 var umur int = 30
 
 func main() {
-	// komentar
 	namaDepan = "John"
 	namaBelakang = "Doe"
-	// umur = 30
-	// ini adalah
-	// komentar multibaris
+
 	fmt.Println(namaDepan + " " + namaBelakang)
 }
