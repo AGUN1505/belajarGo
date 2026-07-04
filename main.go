6f84e1332e3f11885229ac9f345cc1e133f2e64e
Agun Firmansyah <64359161+AGUN1505@users.noreply.github.com>
2026-07-04 10:26:18 +0700

    adding const

--- main.go
+++ main.go
@@ -6,9 +6,13 @@ var namaDepan string
 var namaBelakang string
 var umur int = 30
 
+const pi float64 = 3.14
+
 func main() {
 	namaDepan = "John"
 	namaBelakang = "Doe"
 
 	fmt.Println(namaDepan + " " + namaBelakang)
+	fmt.Println("Umur:", umur)
+	fmt.Println("Nilai pi:", pi)
 }
